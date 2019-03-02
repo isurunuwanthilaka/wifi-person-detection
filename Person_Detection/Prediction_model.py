4273917def8e788bf7432671251f20b0ea3a6a71
Isuru Nuwanthilaka <isurunuwanthilaka@gmail.com>
2019-03-02 16:29:23 +0530

    Predict

--- Person_Detection/Prediction_model.py
+++ Person_Detection/Prediction_model.py
@@ -5,7 +5,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, confusion_matrix 
 
 # 2. Import unknoen data
-wifidata = pd.read_csv("./cleanedDataSet.csv")
+wifidata = pd.read_csv("./unknownData.csv")
 
 # 3. load the model from disk
 filename = 'finalized_model.sav'
