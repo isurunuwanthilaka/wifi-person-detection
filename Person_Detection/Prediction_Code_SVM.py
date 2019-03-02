c20d28e24d22365c357138eb52aed014b04f93fa
Isuru Nuwanthilaka <isurunuwanthilaka@gmail.com>
2019-03-02 16:22:05 +0530

    Save model and load

--- Person_Detection/Prediction_Code_SVM.py
+++ Person_Detection/Prediction_Code_SVM.py
@@ -3,7 +3,8 @@ import json
 import csv
 import os
 import pandas as pd
-import numpy as np  
+import numpy as np 
+import pickle 
 from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split  
 from sklearn.metrics import classification_report, confusion_matrix 
@@ -12,9 +13,7 @@ from sklearn.metrics import classification_report, confusion_matrix
 # 2. Importing the dataset
 #Download the dataset from database as a CSV file and store in the local directory. 
 #To read data from CSV file, the simplest way is to use read_csv method of the pandas library. 
-wifidata = pd.read_csv("wifi_dataggggg.csv")   #chnage your file name
-unknowndata = pd.read_csv("unknownData.csv")
-
+wifidata = pd.read_csv("./cleanedDataSet.csv")   #change your file name
 
 # 3. Exploratory Data Analysis
 #check the dimensions of the data and see first few records
@@ -45,48 +44,6 @@ print(confusion_matrix(y_test,y_pred))
 print("\nClassification Report:")
 print(classification_report(y_test,y_pred))
 
-# 8. Predict Unknown Data
-unknown_pred = svclassifier.predict(unknowndata)
-
-
-# ---------- Remaining part is the Presentation of Data ---------------------#
-
-# 9. Create a Text File
-file = open("Result_Group3.txt", "w")
-print("\nUnknown Data Labels")
-for i in range(len(unknown_pred)):
-    result= "Predicted room id for the respective row " + str(i+1) + " is " + str(unknown_pred[i])
-    file.write(result)
-    file.write("\n")
-    print(result)
-file.close()
-
-# 10. Create a Csv File
-val=0
-def data_store_csv():
-
-    result = {"UoM_Wireless1": -100, "UoM_Wireless6": -100, "UoM_Wireless11":-100, "eduroam1": -100, "eduroam6": -100, "eduroam11": -100, "Jungle Book10":-100, "PROLINK_H5004NK_8766E11": -100,"UNIC-wifi11":-100}
-    ids=["UoM_Wireless1","UoM_Wireless6","UoM_Wireless11","eduroam1","eduroam6","eduroam11","Jungle Book10","PROLINK_H5004NK_8766E11","UNIC-wifi11"]
-
-    filename = "Result_Group3.csv"
-    count=0
-    for y in row:
-        result[ids[count]]=y
-        count+=1
-    result["id"] = unknown_pred[val]
-    df = pd.Series(result).to_frame().T
-    df.to_csv(filename,index=False,mode='a',header=(not os.path.exists(filename)))
-    
-
-with open('unknownData.csv', newline='', encoding='utf-8') as f:
-    reader = csv.reader(f)
-    for row in reader:
-        try:
-            tr=int(row[0])
-            data_store_csv()
-            val+=1
-        except:
-            continue
-
-
-
+# 8.save the model to disk
+filename = 'finalized_model.sav'
+pickle.dump(svclassifier, open(filename, 'wb'))
---
+++ Person_Detection/Prediction_model.py
@@ -0,0 +1,16 @@
+# 1. Importing libraries
+import pickle 
+import pandas as pd
+from sklearn.model_selection import train_test_split  
+from sklearn.metrics import classification_report, confusion_matrix 
+
+# 2. Import unknoen data
+wifidata = pd.read_csv("./cleanedDataSet.csv")
+
+# 3. load the model from disk
+filename = 'finalized_model.sav'
+model = pickle.load(open(filename, 'rb'))
+
+# 4. Making Predictions
+y_pred = model.predict(wifidata)
+print(y_pred)
