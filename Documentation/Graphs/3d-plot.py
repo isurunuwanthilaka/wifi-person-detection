6c3952a6db88faf73f332670894e6e1101a8f76a
Isuru Nuwanthilaka <isurunuwanthilaka@gmail.com>
2019-03-20 00:16:16 +0530

    Add 6D view

--- Documentation/Graphs/3d-plot.py
+++ Documentation/Graphs/3d-plot.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-from matplotlib.colors import ListedColormap
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib as mpl
 import numpy as np
@@ -8,8 +7,8 @@ import seaborn as sns
 
 # Visualizing 3-D mix data using scatter charts
 # leveraging the concepts of hue, size, depth and shape
-fig = plt.figure(figsize=(-100,-100))
-t = fig.suptitle('Chamidi - NipunaM - IsuruAp - UoM_wireless - UNIC-wifi', fontsize=14)
+fig = plt.figure(figsize=(50,50))
+t = fig.suptitle('Chamidi - NipunaM - IsuruAp - Sandali - UoM_wireless - UNIC-wifi', fontsize=14)
 ax = fig.add_subplot(111, projection='3d')
 
 # Loading data
@@ -19,18 +18,16 @@ data = pd.read_csv(filename)
 xs = list(data['Chamidi1'])
 ys = list(data['NipunaM6'])
 zs = list(data['IsuruAp6'])
-data_points = [(x, y, z) for x, y, z in zip(xs, ys, zs)]
 
-ss = list(map(int,-data['UoM_Wireless11']))
-colors = list(map(int,-data['UNIC-wifi11']))
-markers = [',' if q == 0 else 'x' if q == 1 else 'o' for q in list(data['id'])]
+ss = np.array([int(abs(q)) for q in list(data['UoM_Wireless11'])])
+colors = np.array([ 'red' if q==0 else 'green' if q==1 else 'blue' for q in list(data['id'])])
 
-for data, color, size, mark in zip(data_points, colors, ss, markers):
-    x, y, z = data
-    ax.scatter(x,y,z,alpha=0.4, c= '0.'+str(130-color), edgecolors='none', s=(size**1.2), marker=mark)
+ax.scatter(xs, ys, zs, c=colors, s=ss)
 
 ax.set_xlabel('Chamidi1')
 ax.set_ylabel('NipunaM6')
 ax.set_zlabel('IsuruAp6')
-plt.legend()
+
 plt.show()
+
+
